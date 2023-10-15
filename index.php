e5daea7cbee53c7713992d646c6d5c5f22a00ad8
Shinichiro-Nuno <nuno1216shin@gmail.com>
2023-10-15 19:09:37 +0900

    アウトライン作成

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?php echo "Hello PHP";
+<?php
+
+if(!empty($_POST["submitButton"])) {
+    echo $_POST["username"];
+    echo $_POST["comment"];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -6,9 +12,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP掲示板</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+    <h1 class="title">PHPで掲示板アプリ</h1>
+    <hr>
+    <div class="boardWrapper">
+        <section>
+            <article>
+                <div class="wrapper">
+                    <div class="nameArea">
+                        <span>名前：</span>
+                        <p class="username">Shinichiro</p>
+                        <time>:2023/10/15</time>
+                    </div>
+                    <p class="comment">手書きコメントです。</p>
+                </div>
+            </article>
+        </section>
+        <form class="formWrapper" method="POST">
+            <div>
+                <input type="submit" value="書き込む" name="submitButton">
+                <label for="">名前：</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <textarea class="commentTextArea" name="comment"></textarea>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
